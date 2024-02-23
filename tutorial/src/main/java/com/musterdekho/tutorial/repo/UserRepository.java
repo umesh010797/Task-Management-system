a29a263c42cdcb61ec5dff07c07dd28a800045de
Umesh Tandulkar <101276891+umesh010797@users.noreply.github.com>
2024-02-23 23:16:30 +0530

    Delete tutorial directory

--- tutorial/src/main/java/com/musterdekho/tutorial/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.musterdekho.tutorial.repo;
-
-import com.musterdekho.tutorial.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
